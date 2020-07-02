58221719ba7c84159d5448a2d22878b7093a8b7f
Daniel Aranda Martin <Daniel_Aranda_Mar88@hotmail.com>
2020-07-01 20:47:06 -0500

    Agregando servicio para crear token de autenticación

---
+++ api/services/jwt.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const jwt = require('jwt-simple');
+const moment = require('moment');
+const secret_key = 13795523445;
+
+exports.createToken = function(user){
+    let payload = {
+        name: user.name,
+        surname: user.surname,
+        nickname: user.nickname,
+        email: user.email,
+        role: user.role,
+        iat: moment().unix(),
+        exp: moment().add(2, 'days').unix()
+    }
+
+    return jwt.encode(payload, secret_key);
+}
