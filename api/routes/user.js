9a83d17e5e1fd8dc1f71c82fdf8b330fe578d039
Daniel Aranda Martin <Daniel_Aranda_Mar88@hotmail.com>
2020-07-08 16:08:45 -0500

    Agregando rutas para editar la cuenta del usuario

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,11 +3,24 @@
 const express = require('express');
 const api = express.Router();
 const UserController = require('../controllers/user');
+const auth_md = require('../middlewares/authenticated');
 
 // RUTA DE REGISTRO
 api.post('/user/register', UserController.register);
 
 // RUTA DE AUTENTICACIÓN
 api.post('/user/login', UserController.login);
+
+// actualizar infromación básica del usuario
+api.put('/user/update-general-data', auth_md.auth, UserController.updateUserGeneralData);
+
+// ACTUALIZAR NOMBRE DE USUARIO
+api.put('/user/update-nickname', auth_md.auth, UserController.updateUserNickname);
+
+// ACTUALIZAR LA CONTRASEÑA DEL USUARIO
+api.put('/user/update-password', auth_md.auth, UserController.updateUserPassword);
+
+// ACTUALIZAR CORREO ELECTRÓNICO DEL USUARIO
+api.put('/user/update-email', auth_md.auth, UserController.updateEmail);
 
 module.exports = api;
