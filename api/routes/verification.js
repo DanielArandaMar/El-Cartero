2954d7756ff09d9fd2b640da46094a6be5167094
Daniel Aranda Martin <Daniel_Aranda_Mar88@hotmail.com>
2020-07-08 17:29:27 -0500

    Agregando funcionalidad de eliminación del documento de verifiación de cuenta

--- api/routes/verification.js
+++ api/routes/verification.js
@@ -2,7 +2,10 @@
 
 const express = require('express');
 const api = express.Router();
-/** Incluir el controllador */
+const auth_md = require('../middlewares/authenticated');
+const VerificationController = require('../controllers/verification');
 
+/* ELIMINAR DOCUMENTO DE VERIFICACION */
+api.delete('/verification/delete/:accountId', auth_md.auth, VerificationController.deleteVerification);
 
 module.exports = api;
