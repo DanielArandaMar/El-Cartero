e11b7ad1144dd772db0f4e1ee6cc7132ad000963
Daniel Aranda Martin <Daniel_Aranda_Mar88@hotmail.com>
2020-07-17 20:42:04 -0500

    Agregando rutas del api para el correo

---
+++ api/controllers/account.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const HttpResponses = require('../services/httpResponses.js');
+const Account = require('../models/account.js');
+const Verification = require('../models/verification.js');
+
+const controller = {
+
+	/**  CAMBIAR LA CUENTA DE INACTIVA A ACTIVA **/
+	changeToActive: function(req, res){
+		const insertedCode = req.body.code;
+		const userId = req.user.sub;
+
+		Account.findOne({ user: userId }, (err, account) => {
+			if(err) return HttpResponses.display500Error(res);
+			if(!account) return HttpResponses.display400Error(res);
+			Verification.findOne({ account: account._id }, (err, verification) => {
+				if(err) return HttpResponses.display500Error(res);
+				if(!verification) return HttpResponses.display400Error(res);
+
+				// Comparar los dos códigos de verificación
+				if(insertedCode == verification.code){
+					
+					// FALSE -> TRUE Actualizar
+					Account.update({ _id: account._id }, {active: true}, {new: true}, (err, accActive) => {
+						if(err) return HttpResponses.display500Error(res);
+						if(!accActive) return httpResponses.displayCustom(res, 400, 'Algo salió mal en tu verificación.');
+
+						// Eliminar documento de verificación
+						Verification.remove({_id: verification._id}, (err, deletedVerification) => {
+							if(err) return HttpResponses.display500Error(res);
+							if(!verification) return HttpResponses.display400Error(res);
+
+							return res.status(200).send({ account: accActive });
+						});
+
+					});
+				} else {
+					return HttpResponses.displayCustom(res, 400, 'Código de verificación incorrecta.');
+				}
+			});
+		});
+	}
+
+}
+
+module.exports = controller;
--- api/routes/mail.js
+++ api/routes/mail.js
@@ -1,8 +1,22 @@
 'use strict'
 
 const express = require('express');
+const multiparty = require('connect-multiparty');
 const api = express.Router();
-/** Incluir el controllador */
+const md_auth = require('../middlewares/authenticated');
+const md_upload = multiparty({uploadDir: './uploads/image-mails'});
+const MailController = require('../controllers/mail');
 
+// ENVIAR UN CORREO
+api.post('/mail/send', md_auth.auth, MailController.sendMail);
+
+// ENVIAR IMAGEN ADJUNTA EN EL correo
+api.post('/mail/upload-image/:id', [md_auth.auth, md_upload], MailController.uploadImage);
+
+// OBTENER MIS MAILS PAGINADOS
+api.get('/mail/my-mails/:page?', md_auth.auth, MailController.getMyMails);
+
+// OBTENER UN CORREO EN CONCRETO
+api.get('/mail/single/:id', md_auth.auth, MailController.getMail);
 
 module.exports = api;
